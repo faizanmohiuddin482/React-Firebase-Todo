2ba052238b02378a436d32de79aec1ab1d2f3070
Mohammed Faizan Mohiuddin <faizanmohiuddin482@gmail.com>
2020-10-08 22:52:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  // useState is temporary, as soon as we refresh , it's gone
+  const [todos, setTodos] = useState(['Take dogs for a walk','Take dogs for a walk again']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Utilosoft Learn </h1>
+      <input />
+      <button>Add Todo</button>
+      <ul>
+        {/* the curly braces are from jsx and the .map is from es6 . map is used for looping */}
+        {todos.map}
+        <li></li>    
+      </ul>
     </div>
   );
 }
